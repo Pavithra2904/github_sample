b599bb05d276904dbeff2b09e94462d0c1d0ad7c
pavithra <pavigopi2004@gmail.com>
2024-10-16 10:20:50 +0530

    First Commit

---
+++ ratings-counter.py
@@ -0,0 +1,14 @@
+from pyspark import SparkConf, SparkContext
+import collections
+print("Github tutorial")
+
+conf = SparkConf().setMaster("local").setAppName("RatingsHistogram")
+sc = SparkContext(conf = conf)
+
+lines = sc.textFile("file:///SparkCourse/movielens/data/u.data")
+ratings = lines.map(lambda x: x.split()[2])
+result = ratings.countByValue()
+
+sortedResults = collections.OrderedDict(sorted(result.items()))
+for key, value in sortedResults.items():
+    print("%s %i" % (key, value))
